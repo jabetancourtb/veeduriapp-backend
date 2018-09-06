23050baa47d952f70aae3881a6135f181225babe
Anderson Betancourt <jabetancourtb@hotmail.com>
2018-09-06 01:47:59 -0500

    Cambios en el registro de usuario y publicaciones

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+  
+    def configure_permitted_parameters
+      #devise_parameter_sanitizer.for(:sign_up){ |u| u.permit(:name, :cc)}
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :cc])
+    end
 end
--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,8 +1,19 @@
 class PublicationsController < ApplicationController
-    def index
 
+    def index
         if user_signed_in?
             @publications = Publication.order("updated_at DESC")
+            
+            
+            # @pub_id = Publication.select("id")
+            # current_user.id
+            # @publication_user = User.joins(:publication).select("name")
+            # @publication_user = User.select(User.arel_table[:name]).where(Publication.arel_table[:user_id].eq(@pub_id)).joins(:publication)
+            # User.select(User.arel_table[:name]).where(Publication.arel_table[:user_id]).joins(:publications)
+            # @publication_user = Publication.joins(:user).select("name")
+
+            # puts @publication_user
+
         elsif
             redirect_to '/'  
         end
@@ -10,12 +21,36 @@ class PublicationsController < ApplicationController
     
 
     def show
-
         if user_signed_in?
             @publications = Publication.find(params[:id])
         elsif
             redirect_to '/'  
         end
-
     end
+
+
+    def new
+        if user_signed_in?
+            @publications = Publication.new
+        elsif
+            redirect_to '/'  
+        end
+    end
+
+
+    def create
+        if user_signed_in?
+            @publications = Publication.new(title: params[:publication][:title], 
+            description: params[:publication][:description],
+            information: params[:publication][:information],
+            photo: params[:publication][:photo],
+            user_id: current_user.id)
+
+            @publications.save
+            redirect_to @publications
+        elsif
+            redirect_to '/'  
+        end
+    end
+
 end
